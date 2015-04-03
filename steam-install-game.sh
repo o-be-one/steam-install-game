c8520c82b05e384f91c765bb2313f9b5ccbf819d
Meddy B. <o_be_one@r0x.fr>
2015-04-03 02:07:31 -0400

    Translation

--- steam-install-game.sh
+++ steam-install-game.sh
@@ -8,45 +8,45 @@
 ######
 
 
-### Variables a editer :
+### Vars to edit :
 
 # What is your Steam account ?
 USER="your-steam-username"
 PASS="your-steam-password"
 
-# Emplacement de Steam :
+# SteamCMD path :
 DEFSTEAM=~/steamcmd
 
-# Ou installer le jeu ?
+# Where to install games ?
 DEFWHERE=~
 
 
-### Editer ce qui suit a vos risques ...
+### Edit ... at your own risks.
 
-echo -e "\nCe script va installer un jeu/serveur Steam.\n"
+echo -e "\nThis script will install Steam game\n"
 
-echo "Ou est installer SteamCMD ? [$DEFSTEAM]"
+echo "Where is SteamCMD ? [$DEFSTEAM]"
 read STEAM
 [[ -z $STEAM ]] && STEAM=$DEFSTEAM
 
-echo "Quel est le nom du jeu (creera le dossier du meme nom) ?"
+echo "What is the name of your game (will create folder with this name) ?"
 read GAME
 
 until [[ $ID =~ ^-?[0-9]+$ ]]
 do
-        echo "Quel est l'ID Steam du jeu a installer ?"
+        echo "What is the game Steam AppID ?"
         read ID
 done
 
-echo "Dans quel dossier installer $GAME (n'oubliez pas, le dossier $GAME sera cree dedans) ? [$DEFWHERE]"
+echo "Where to setup $GAME (don't forget, the folder $GAME will be created in) ? [$DEFWHERE]"
 read WHERE
 [[ -z $WHERE ]] && WHERE=$DEFWHERE
 
-echo "Un argument a ajouter au setup ?"
+echo "Add some setup arguments ?"
 read SUPP
 
-echo -e "\nOk, installation de $GAME (ID : $ID) dans $WHERE/$GAME ..."
-read -p "Appuyez sur [ENTREE] pour demarrer l'installation ..."
+echo -e "\nOk, installation of $GAME (ID : $ID) in $WHERE/$GAME ..."
+read -p "Press [ENTER] to start setup ..."
 
 mkdir -p $WHERE/$GAME
 cd $STEAM && ./steamcmd.sh +login $USER $PASS +force_install_dir $WHERE/$GAME +app_update $ID $SUPP +exit && cd $WHERE/$GAME
